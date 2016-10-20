6dde35a8352c2ee1f6654c1d38d3c2271848abbe
lubien <lubien1996@gmail.com>
2016-10-20 02:01:36 -0300

    Test bookmark

---
+++ test/bookmark_test.exs
@@ -0,0 +1,39 @@
+defmodule BookmarkTest do
+  use ExUnit.Case
+
+  import Bookmarker.Bookmark, only: [ drop_at: 2 ]
+
+  @fixture %{
+    "name" => "Other bookmarks",
+    "children" => [
+      %{
+        "name" => "A",
+        "children" => [
+          %{
+            "name" => "B",
+            "children" => [
+              %{
+                "name" => "C",
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+
+  test "can filter folders" do
+    assert drop_at(@fixture, ["A"]) == %{
+      "name" => "Other bookmarks",
+      "children" => []
+    }
+
+    assert drop_at(@fixture, ["A", "B"]) == %{
+      "name" => "Other bookmarks",
+      "children" => [%{
+        "name" => "A",
+        "children" => [],
+      }]
+    }
+  end
+end
