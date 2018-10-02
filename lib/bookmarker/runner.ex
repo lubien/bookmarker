372de8d58ffe3aa278a7d20712fb3bfe941bc335
Fredrik Leijon <fredrik.leijon@gmail.com>
2018-10-02 19:40:51 +0200

    Include bookmarks in bookmark bar in output

--- lib/bookmarker/runner.ex
+++ lib/bookmarker/runner.ex
@@ -12,35 +12,52 @@ defmodule Bookmarker.Runner do
 
   def get_bookmarks(file) do
     file
-    |> Path.expand
-    |> File.read!
-    |> Poison.decode!
-    |> Map.fetch!("roots")
-    |> Map.fetch!("other")
+    |> Path.expand()
+    |> File.read!()
+    |> Poison.decode!()
+    |> get_other_with_bookmark_bar
+  end
+
+  defp get_other_with_bookmark_bar(bookmark_map) do
+    roots =
+      bookmark_map
+      |> Map.fetch!("roots")
+
+    other = Map.fetch!(roots, "other")
+
+    %{
+      "children" =>
+        Map.fetch!(other, "children") ++
+          if(Map.has_key?(roots, "bookmark_bar"),
+            do: [Map.fetch!(roots, "bookmark_bar")],
+            else: []
+          )
+    }
   end
 
   def ignore_paths(bookmarks, paths) do
     paths
     |> Enum.reduce(bookmarks, fn path, acc ->
-      Bookmark.drop_at acc, String.split(path, "/")
+      Bookmark.drop_at(acc, String.split(path, "/"))
     end)
   end
 
   def restrict_to(bookmarks, path) do
-      Bookmark.get_at bookmarks, path
+    Bookmark.get_at(bookmarks, path)
   end
 
   def render_markdown(bookmarks, config) do
-    Template.render bookmarks, config
+    Template.render(bookmarks, config)
   end
 
   def output(rendered, path)
-  when is_binary(path) do
-    dest = Path.expand path
-    File.write! dest, rendered
-    IO.puts "Saved output at #{dest}"
+      when is_binary(path) do
+    dest = Path.expand(path)
+    File.write!(dest, rendered)
+    IO.puts("Saved output at #{dest}")
   end
+
   def output(rendered, _target) do
-    IO.puts rendered
+    IO.puts(rendered)
   end
 end
--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule Bookmarker.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bookmarker,
-     escript: escript_config(),
-     default_task: "escript.build",
-     version: "1.0.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :bookmarker,
+      escript: escript_config(),
+      default_task: "escript.build",
+      version: "1.0.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
@@ -17,10 +19,10 @@ defmodule Bookmarker.Mixfile do
   end
 
   defp deps do
-    [{:poison, "~> 3.0"}]
+    [{:poison, "~> 4.0"}]
   end
 
   defp escript_config do
-    [ main_module: Bookmarker.CLI ]
+    [main_module: Bookmarker.CLI]
   end
 end
