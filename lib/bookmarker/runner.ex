bef9b94e68fe51d24a3a46d0d721eda3258e2aa3
Jonas Thuresson <thuresson.j@gmail.com>
2019-10-17 19:27:06 +0200

    finished sorting functions

--- lib/bookmarker/runner.ex
+++ lib/bookmarker/runner.ex
@@ -1,33 +1,58 @@
 defmodule Bookmarker.Runner do
   alias Bookmarker.{Bookmark, Template}
 
+  @valid_sort_strategies ~w(name-asc name-desc bookmark-count-asc bookmark-count-desc)
+  def valid_sort_strategies(), do: @valid_sort_strategies
+
   def run(config) do
     get_bookmarks(config.file)
     |> ignore_paths(config.ignore)
     |> restrict_to(config.path)
-    |> order_bookmarks(config.sort)
+    |> sort_bookmarks(config.sort, config.sortby)
     |> render_markdown(Map.take(config, [:title, :description, :timestamp?]))
     |> output(config.output)
   end
 
-  @spec order_bookmarks(any, any) :: any
-  defp order_bookmarks(bookmarks, true), do: order_bookmarks(bookmarks)
-  defp order_bookmarks(bookmarks, false), do: bookmarks
+  defp sort_bookmarks(bookmarks, false, nil), do: bookmarks
+  defp sort_bookmarks(bookmarks, true, nil), do: sort_bookmarks(bookmarks, "name-asc")
+  defp sort_bookmarks(bookmarks, _, strategy), do: sort_bookmarks(bookmarks, strategy)
 
-  defp order_bookmarks(%{"children" => children} = bookmarks) do
-    IO.inspect(bookmarks)
-    sorted_children = Enum.sort_by(
-      order_bookmarks(children),
-      &({Map.fetch!(&1, "name"), Map.fetch!(&1, "type")}),
-      fn {name1, type1}, {name2, type2} ->
-        (type1 != type2 && type1 != "folder") || (type1 == type2 && String.downcase(name1) < String.downcase(name2))
-      end
-    )
+  defp sort_bookmarks(%{"children" => children} = bookmarks, strategy) do
+    sorted_children = sort_bookmarks(children, strategy)
+    |> sort_bookmarks_by(strategy)
     Map.put(bookmarks, "children", sorted_children)
   end
-  defp order_bookmarks([bookmark | rest]), do: [order_bookmarks(bookmark) | order_bookmarks(rest)]
-  defp order_bookmarks([]), do: []
-  defp order_bookmarks(bookmark), do: bookmark
+  defp sort_bookmarks([bookmark | rest], strategy), do: [sort_bookmarks(bookmark, strategy) | sort_bookmarks(rest, strategy)]
+  defp sort_bookmarks([], _), do: []
+  defp sort_bookmarks(bookmark, _), do: bookmark
+
+  defp sort_bookmarks_by(bookmarks, "name-asc"), do: sort_bookmarks_by(bookmarks, :name, &(</2))
+  defp sort_bookmarks_by(bookmarks, "name-desc"), do: sort_bookmarks_by(bookmarks, :name, &(>/2))
+  defp sort_bookmarks_by(bookmarks, "bookmark-count-asc"), do: sort_bookmarks_by(bookmarks, :bookmarkcount, &(</2))
+  defp sort_bookmarks_by(bookmarks, "bookmark-count-desc"), do: sort_bookmarks_by(bookmarks, :bookmarkcount, &(>/2))
+
+  defp sort_bookmarks_by(bookmarks, :name, comparer) do
+    Enum.sort_by(
+      bookmarks,
+      &({Map.fetch!(&1, "name"), Map.fetch!(&1, "type")}),
+      fn {name1, type1}, {name2, type2} ->
+        (type1 != type2 && type1 != "folder") || (type1 == type2 && comparer.(String.downcase(name1), String.downcase(name2)))
+      end
+    )
+  end
+  defp sort_bookmarks_by(bookmarks, :bookmarkcount, comparer) do
+    Enum.sort_by(
+      bookmarks,
+      &({Map.fetch!(&1, "name"), Map.fetch!(&1, "type"), &1}),
+      fn {name1, type1, bookmark1}, {name2, type2, bookmark2} ->
+        len1 = Map.get(bookmark1, "children", []) |> length
+        len2 = Map.get(bookmark2, "children", []) |> length
+        (type1 != type2 && type1 != "folder")
+        || (type1 == "folder" && type2 == "folder" && comparer.(len1, len2))
+        || (type1 == type2 && String.downcase(name1) < String.downcase(name2))
+      end
+    )
+  end
 
   @spec get_bookmarks(
           binary
